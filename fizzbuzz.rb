46505decd918b1df285d952c099f0746633aafef
Diana Utan <dia_utzi@yahoo.com>
2019-02-11 21:21:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(number)
-  if number % 15  == 0
-    return  "FizzBuzz"
+  if number % 3  == 0
+    return  "Fizz"
     elsif number % 5 == 0
     return "Buzz"
-  elsif number % 3== 0
-  return "Fizz"
+  else number % 15 == 0
+  return "FizzBuzz"
   end
 end
